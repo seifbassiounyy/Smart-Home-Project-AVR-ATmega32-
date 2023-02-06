0dc6dffc305e6fc4b158d00689ebc66513e425f9
Seif Bassiouny <104737465+seifbassiounyy@users.noreply.github.com>
2023-02-06 17:17:29 +0200

    Add files via upload

---
+++ Master/COTS/Services/STD_TYPES.h
@@ -0,0 +1,18 @@
+////Author: Seif Bassiouny
+////Date: 4/9/2022
+////Version: 1.1
+////Module: Services 
+
+#ifndef STD_H_
+#define STD_H_
+
+typedef unsigned char Uint8;
+typedef signed char Sint8;
+typedef unsigned long int Uint32;
+typedef unsigned short int Uint16;
+typedef signed long int Sint32;
+typedef signed short int Sint16;
+typedef float Fint32;
+typedef double Fint64;
+
+#endif /* STD_H_ */
